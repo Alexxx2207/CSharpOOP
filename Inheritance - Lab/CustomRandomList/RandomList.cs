571d5a055535cc3dd17c8490abbe862f84b1ca97
Alexxx2207 <alex.ivanov0722@gmail.com>
2021-11-04 16:22:18 +0200

    Inheritance Lab Added

---
+++ Inheritance - Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {
+        public string RandomString() 
+        {
+            Random rand = new Random();
+            int randNum = rand.Next(0, this.Count);
+            string word = this.GetRange(randNum, 1).ToString();
+            this.RemoveAt(randNum);
+            return word;
+        }
+    }
+}
